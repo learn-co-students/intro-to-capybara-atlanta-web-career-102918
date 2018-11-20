27efade6a1cab8597df123cf046167c31b29bef8
Cory <coadams9@gmail.com>
2018-11-20 16:17:52 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 class Application < Sinatra::Base
-  # Write your code here!
+  get '/' do
+    erb :index
+  end
+
+  post '/greet' do
+    erb :greet
+  end
+
+
+
+
+
+
+
+
+
+
+
 
 end
--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -26,3 +26,16 @@ describe "POST '/greet' - User Greeting" do
     expect(page).to have_text("Hi Avi, nice to meet you!")
   end
 end
+
+describe "POST '/greet' - User Greeting" do
+  it 'greets the user personally based on their user_name in the form' do
+    visit '/'
+
+    fill_in(:user_name, :with => "Avi")
+    click_button "Submit"
+
+    expect(page).to have_text("Hi Avi, nice to meet you!")
+  end
+
+
+end
